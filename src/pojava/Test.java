ba04c077c07b09b7a1f116e0cf0a2efd15de8ac5
zofiamaj <zoja_maj@wp.pl>
2020-03-21 18:16:00 +0100

    dodałam Test.java - ucze się gita na razie, sokri :)

---
+++ src/pojava/Test.java
@@ -0,0 +1,13 @@
+package pojava;
+
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello, czy to dziala??");
+	}
+
+}
