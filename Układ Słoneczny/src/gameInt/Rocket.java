deb3bd23af9de511f823dc9ff6b12126e5bd75fa
lidka-lappo <62439683+lidka-lappo@users.noreply.github.com>
2020-03-31 12:47:57 +0200

    Create Rocket.java

---
+++ Układ Słoneczny/src/gameInt/Rocket.java
@@ -0,0 +1,54 @@
+\\Na razie  bez rysowania i zmian w prędkości i położeniu
+import java.awt.Color;
+import java.awt.Point;
+
+public class Rocket {
+	Point apex, middleOfBase;
+	int height, widthOfBase; 
+	Color rocketColor;
+	int rocketMass;
+	float forceX, forceY, accelerationX, accelerationY, velocityX, velocityY;
+	
+	public Rocket(Point ap, Point mOB, int h, int wOB, Color clr, int m, float Fx, float Fy, float ax, float ay, float Vx, float Vy )
+	{
+		apex =ap;
+		middleOfBase =mOB;
+		height =h;
+		widthOfBase =wOB;
+		rocketColor =clr;
+		rocketMass = m;	
+		forceX = Fx;
+		forceY = Fy;
+		accelerationX = ax;
+		accelerationY = ay;
+		velocityX = Vx;
+		velocityY =Vy;
+	}
+	Point getApex ()
+	{
+		return apex;
+	}
+	Point getMiddleOfBase ()
+	{
+		return middleOfBase;
+	}
+	Color getColor ()
+	{
+		return rocketColor;
+	}
+	int getMass ()
+	{
+		return rocketMass;
+	}
+	int getHeight ()
+	{
+		return height;
+	}
+	int getWidthOfBase ()
+	{
+		return widthOfBase;
+	}
+
+	
+	
+}
