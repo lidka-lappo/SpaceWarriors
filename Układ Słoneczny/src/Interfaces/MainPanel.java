cd18366767fb0030075b7b607cbd45aa6100a7aa
lidka-lappo <62439683+lidka-lappo@users.noreply.github.com>
2020-04-15 14:27:36 +0200

    Rakietka
    Bez silników, za to czuła na grawitacje <3

--- Układ Słoneczny/src/Interfaces/MainPanel.java
+++ Układ Słoneczny/src/Interfaces/MainPanel.java
@@ -12,7 +12,8 @@ public class MainPanel extends JPanel implements Runnable {
 	int wid;
 	int heig;
 	public boolean czynny =true;
-	
+	Rocket rocket;
+	Color rocketColor= Color.blue;
 	public MainPanel(int width, int height, Planet pl[]) {
 		
 		wid = width;
@@ -23,6 +24,7 @@ public class MainPanel extends JPanel implements Runnable {
 		mainPanel.setBounds(0, 0, width, height);
 		
 		planets = pl.clone();
+		rocket = new Rocket(new Point(500,300),  rocketColor);
 
 	}
 
@@ -31,32 +33,65 @@ public class MainPanel extends JPanel implements Runnable {
 		super.paintComponent(g);
 		g.setColor(Color.black);//t³o xd
 		g.fillRect(0, 0, wid, heig);
+		rocket.paintRocket(g);
+		
 		// planetki
 		for (int i = 0; i<9; i++) {
 			planets[i].paintPlanet(g);	
 		}
-		/*Graphics2D g2d = (Graphics2D) g;
-		g2d.setColor(Color.red);
-		g2d.fillRect(560, 460, 10, 10);*/
+
 	
 	}
 	
 
 	
 	public void run() {
-		
+
 		double tmpSunDist=0;
 		
 		double tmpA = 0;
 		double tmpAV = 0;
 		
-		int tmpX = 0;
+		int tmpX = 0; 
 		int tmpY = 0;
 		Point tmpPXY;
+		
+		double Dist =0;
+		double DistX =0;
+		double DistY =0;
+		
+		int tmpRX = 0; 
+		int tmpRY = 0;
+		
+		double tmpVX = 0; 
+		double tmpVY = 0;
+		
+		double tmpaX = 0; 
+		double tmpaY = 0;
+
+		double sina =0;
+		double cosa =0;
 
 		while(czynny) {
+			tmpRX=(int)rocket.getApex().getX();
+			tmpRY=(int)rocket.getApex().getY();
+						
+			tmpVX=rocket.getVx();
+			tmpVY=rocket.getVy();
+			
+			DistX= planets[0].getXY().getX() - tmpRX;
+			DistY= planets[0].getXY().getY() - tmpRY;
+			Dist = (int) Math.pow(DistX*DistX+DistY*DistY, 0.5);
+			sina = DistY/Dist;
+			cosa = DistX/Dist;
+			tmpaX = cosa*planets[0].getMass()/(Dist*Dist); 
+		    tmpaY = sina*planets[0].getMass()/(Dist*Dist);
+
+			
+			
 			for (int i = 1; i<9; i++)
 			{
+				//ruch planet
 				tmpSunDist=planets[i].getSDist();
 				
 				tmpA=planets[i].getAngle();
@@ -72,10 +107,44 @@ public class MainPanel extends JPanel implements Runnable {
 				tmpY=(int)planets[0].getXY().getY()+(int) (tmpSunDist*Math.cos(tmpA));
 	
 				tmpPXY = new Point(tmpX, tmpY);
-				planets[i].setXY(tmpPXY);
+				planets[i].setXY(tmpPXY);	
+				
+				//ruch rakiety	
+				DistX= tmpX - tmpRX;
+				DistY= tmpY - tmpRY;
+				Dist = (int) Math.pow(DistX*DistX+DistY*DistY, 0.5);
+				sina = DistY/Dist;
+				cosa = DistX/Dist;
+				
+				
+				
+				tmpaX= tmpaX + cosa*planets[i].getMass()/(Dist*Dist);
+				tmpaY= tmpaY + sina*planets[i].getMass()/(Dist*Dist);
+				
 				
 			}
+			//ruch rakiety
+			tmpVX+=tmpaX;
+			tmpVY+=tmpaY;
+			if(tmpVX>10) //ograniczenie prędkości światła
+				tmpVX=10;
+			if(tmpVX<-10)
+				tmpVX=-10;
+			
+			if(tmpVY>10)
+				tmpVY=10;
+			if(tmpVY<-10)
+				tmpVY=-10;
 
+			tmpRX+=tmpVX;
+			tmpRY+=tmpVY;
+			
+			tmpPXY = new Point(tmpRX, tmpRY);
+			rocket.setApex(tmpPXY);	
+			
+			rocket.setVx(tmpVX);	
+			rocket.setVy(tmpVY);	
+			
 			this.repaint();
 		
 			try {
---
+++ Układ Słoneczny/src/Interfaces/Rocket.java
@@ -0,0 +1,87 @@
+package Interfaces;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Polygon;
+
+
+
+public class Rocket {
+
+	Point apex;
+	
+	Color color;
+
+	double Vx;
+	double Vy;
+
+	
+	public Rocket(Point a,  Color col) {
+		color = col;
+		apex =a;
+		Vx=0; //nazwy do poprawy kiedys tam
+		Vy=0;
+	}
+	
+	Point getApex ()
+	{
+		return apex;
+	}
+	
+	void setApex (Point a)
+	{
+		this.apex = a;
+	}
+	
+	
+	Color getColor ()
+	{
+		return color;
+	}
+	
+	void setColor(Color col)
+	{
+		this.color =col;
+	}
+
+	double getVx ()
+	{
+		return Vx;
+	}
+	
+	void setVx(double Vx)
+	{
+		this.Vx =Vx;
+	}
+	
+	double getVy ()
+	{
+		return Vy;
+	}
+	
+	void setVy(double Vy)
+	{
+		this.Vy =Vy;
+	}
+	
+	
+	
+	
+	void paintRocket(Graphics g) {
+		/* Graphics2D g2d = (Graphics2D) g;
+		 Ellipse2D circle = new Ellipse2D.Double(getApex().getX(), getApex().getY(), 20, 20);
+		 g.setColor(getColor());
+		 g2d.fill(circle);*/
+		
+		Graphics2D g2d = (Graphics2D) g; //Rysowanie trójąta, dla mechaniki na razie rakieto to koło
+		
+		 int x[]= new int[] {(int) getApex().getX(), (int) getApex().getX() -5 , (int) getApex().getX()+5};
+		 int y[]= new int[] {(int) getApex().getY(), (int) getApex().getY()-10, (int) getApex().getY()-10};
+		 Polygon triangle = new Polygon(x, y, 3);
+		 g.setColor(getColor());
+		 g2d.fill(triangle);
+		 g.drawPolygon(triangle);
+	}
+}
